daff2ab8a99944507441bccfb15f4c14a98c26a8
Alexandersam-hub <Alexandersamedov@gmail.com>
2021-11-14 14:15:10 +0300

    Добавьте файлы проекта.

---
+++ Calculater/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculater
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach(UIElement el in MainGrid.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button)el).Click += Button_Click;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string strButton = (string)((Button)e.OriginalSource).Content;         
+            if(strButton == "Ac")
+                TextLabel.Text = "";
+            else if(strButton == "=")
+                TextLabel.Text = new DataTable().Compute(TextLabel.Text, null).ToString();
+           else 
+                TextLabel.Text += strButton;
+        }
+    }
+}
